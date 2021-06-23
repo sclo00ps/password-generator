47b650a1fd0f28104d6732e686d631f7159b6ab2
sclo00ps <louginescott@gmail.com>
2021-06-22 20:29:22 -0400

    variables

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var symbols = [".","\\", ":", ";"];
+console.log(symbols); 
+var str = "abcdefghijklmnopqrstuvwxyz";
+var alphaArray = str.split(""); // the argument is a null string, "".
+var alphaUpperArray = str.toUpperCase().split("");
+var alphaUpperArray = str.toLowerCase().split("");
 
 // Write password to the #password input
 function writePassword() {
