809cb624f320ec70d2602962a37eb45a184fae7d
sclo00ps <louginescott@gmail.com>
2021-08-16 15:32:29 -0700

    generated README.md file

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# password-generator-application
+
+
+    # Title 
+    Password Generator
+
+    # Table of Contents
+
+     - [Description] (#description)
+     - [Installation](#installation)
+     - [Usage](#usage)
+     - [Contribution](#contributions)
+     - [Tests](#tests)
+     - [Credits](#credits)
+     - [License](#license)
+     - [Questions](#questions)
+
+    ## Description
+     [![License](https://img.shields.io/badge/License-MIT-blue.svg "License Badge)]
+
+     Eenables employees to generate random passwords based on criteria that they’ve selected.
+
+    ## Installation:
+     Install inquirer to generate the README.md file. The application is displayed in the Browswer.
+
+     ## Usage:
+     None at this time.
+
+     ## Contribution:
+     Additional work will be done to complete, under time cconstraints at this time.
+
+     ## Tests:
+     Use Browser Inspect tool for error and functional testing.
+
+     ## Credits:
+     Reference link for special characters:  https://owasp.org/www-community/password-special-characters.
+     
+     ## License:
+     MIT
+        For additional licensing information, please click the link displayed below:
+
+        <https://img.shields.io/badge/License-MIT-blue.svg> "License Badge"
+
+        
+     ## Questions:
+        The link below will take you to my Github Profile for additional details:
+
+       <https://github.com/sclo00ps>
+    
+        If there are any additional questions, plse contact me at:  
+        <louginescott@gmail.com>
+
+    
--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var symbols = [".","\\", ":", ";"];
-console.log(symbols); 
+var symbols = [".", "\\", ":", ";"];
+console.log(symbols);
 var str = "abcdefghijklmnopqrstuvwxyz";
 var alphaArray = str.split(""); // the argument is a null string, "".
 var alphaUpperArray = str.toUpperCase().split("");
 var alphaUpperArray = str.toLowerCase().split("");
-var specialCharacters = ["!", "@", "#", "$", "%", "(", ")", "_", "*", "&","^", "+", "-", "/", "=", "/", "[", ",", "{", "}", "]", "//", " .", "\\", "'", "<>"];      
-var alphaLowerArray= ["a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacters = ["!", "@", "#", "$", "%", "(", ")", "_", "*", "&", "^", "+", "-", "/", "=", "/", "[", ",", "{", "}", "]", "//", " .", "\\", "'", "<>"];
+var alphaLowerArray = ["a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Confirm statements for password length
-var pwLength = window.prompt("Your password length should be 8 - 128 characters long, what length do you want your password to be?"); 
+var pwLength = window.prompt("Your password length should be 8 - 128 characters long, what length do you want your password to be?");
 
 // Confirm statements for password character types
-var specChar = confirm("Do you want to include special characters...?@[\]^_?"); 
-var  lowCase = confirm("Do you want to include lower case characters?"); 
-var  uppCase = confirm("Do you want to include upper case characters?"); 
-var  numCase = confirm("Do you want to include numeric characters?"); 
+var specChar = confirm("Do you want to include special characters...?@[\]^_?");
+var lowCase = confirm("Do you want to include lower case characters?");
+var uppCase = confirm("Do you want to include upper case characters?");
+var numCase = confirm("Do you want to include numeric characters?");
 
 
-if (specChar===false && lowCase===false && uppCase===false && numCase ===false) {
-  alert ("Please include at least one of each type of character choice in your password")
+if (specChar === false && lowCase === false && uppCase === false && numCase === false) {
+  alert("Please include at least one of each type of character choice in your password")
   return;
-  }
+}
 
 
 
